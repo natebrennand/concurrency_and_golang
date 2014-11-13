823dd86e9e825a169eccb9b9eb5902de83e3a418
Nate Brennand <natebrennand@gmail.com>
2014-11-13 17:31:46 -0500

    oops, now threadsafe and finishes

--- 100workers/main.go
+++ 100workers/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/natebrennand/concurrency_and_go/cat_util"
 )
@@ -17,23 +18,27 @@ func main() {
 	}
 
 	catPipe := make(chan string)
+	wg := &sync.WaitGroup{}
+
 	for i := 0; i < 100; i++ {
-		go func(pipe chan string) {
+		go func(pipe chan string, w *sync.WaitGroup) {
 			for {
 				cat := <-pipe
-				if cat == "" {
-					continue
-				}
 				err := cat_util.GetCat(cat)
 				if err != nil {
 					log.Printf("Failed to download cat, %s => %s", cat, err.Error())
 				}
 				fmt.Print(".")
+				w.Done()
 			}
-		}(catPipe)
+		}(catPipe, wg)
 	}
 
 	for _, cat := range catList {
-		catPipe <- cat
+		if cat != "" {
+			wg.Add(1)
+			catPipe <- cat
+		}
 	}
+	wg.Wait()
 }
--- 10workers/main.go
+++ 10workers/main.go
@@ -3,34 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/natebrennand/concurrency_and_go/cat_util"
 )
 
 func main() {
+	// Does parallelizing make it faster?
+	// runtime.GOMAXPROCS(runtime.NumCPU())
+
 	catList, err := cat_util.GetList("cats.txt")
 	if err != nil {
 		log.Fatalf("Could not generate list of catList => %s", err.Error())
 	}
 
 	catPipe := make(chan string)
+	wg := &sync.WaitGroup{}
+
 	for i := 0; i < 10; i++ {
-		go func(pipe chan string) {
+		go func(pipe chan string, w *sync.WaitGroup) {
 			for {
 				cat := <-pipe
-				if cat == "" {
-					continue
-				}
 				err := cat_util.GetCat(cat)
 				if err != nil {
 					log.Printf("Failed to download cat, %s => %s", cat, err.Error())
 				}
 				fmt.Print(".")
+				w.Done()
 			}
-		}(catPipe)
+		}(catPipe, wg)
 	}
 
 	for _, cat := range catList {
-		catPipe <- cat
+		if cat != "" {
+			wg.Add(1)
+			catPipe <- cat
+		}
 	}
+	wg.Wait()
 }
